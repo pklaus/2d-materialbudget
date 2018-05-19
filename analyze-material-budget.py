bb88acc3ad500569935b2067b0e4739f64ccf26f
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-05-19 20:11:18 +0200

    analyze script: updated version (also as of 16 Sep 2014)

--- analyze-material-budget.py
+++ analyze-material-budget.py
@@ -13,7 +13,10 @@ class HistoPlot(object):
         """
         raise NotImplementedError()
 
-    def fill(self, values):
+    def fill(self, dict_of_values):
+        """
+        dict_of_values should be like: {'diamond': [0.0, 0.0, 1.4, 1.5, 5.5], }
+        """
         raise NotImplementedError()
 
     def draw(self):
@@ -26,14 +29,29 @@ class MatplotlibPlot(HistoPlot):
         self.plt = plt
         self.np = np
 
-    def fill(self, values):
-        #Stack the data
+        self.n, self.f, self.t = n, f, t
+        self.full_range = self.t - self.f
+        self.stepsize = self.full_range / (self.n + 1)
+
+    def fill(self, dict_of_values):
         #plt.figure()
         fig, ax = self.plt.subplots()
-        x, y = self.np.arange(len(values)), values
-        ax.stackplot(x, y)
+        #first_key = dict_of_values.keys()[0]
+        #x_values = self.np.arange(len(dict_of_values[first_key]))
+        x_values = self.np.linspace(self.f + self.stepsize/2, self.t - self.stepsize/2, self.n)
+        sorted_keys = sorted(dict_of_values.keys())
+        values = [dict_of_values[key] for key in sorted_keys]
+        print(len(x_values), [len(dict_of_values[value]) for value in dict_of_values])
+        polys = ax.stackplot(x_values, *values)
+        # fix for labels in stackplot: http://stackoverflow.com/a/18449695/183995
+        legendProxies = []
+        for poly in polys:
+            legendProxies.append(self.plt.Rectangle((0, 0), 1, 1, fc=poly.get_facecolor()[0]))
+        self.plt.grid('on')
+        self.plt.legend(legendProxies, sorted_keys, loc=2)
 
-    def draw(self):
+    def draw(self, title=""):
+        self.plt.title(title)
         self.plt.show()
 
 class RootPlot(HistoPlot):
@@ -42,6 +60,7 @@ class RootPlot(HistoPlot):
         self.canvas = Canvas()
         self.canvas.SetGrid()
 
+        self.n, self.f, self.t = n, f, t
         self.hist = Hist(n, f, t)
         self.hist.fillstyle = 'solid'
         self.hist.fillcolor = '#87cefa'
@@ -55,13 +74,14 @@ class RootPlot(HistoPlot):
         #self.canvas.Update()
         #self.canvas.Print('./plot.pdf')
 
-    def fill(self, values):
+    def fill(self, dict_of_values):
         i = 0
         for value in values:
             self.hist[i] = value
             i += 1
 
-    def draw(self):
+    def draw(self, title=''):
+        self.hist.SetTitle(title)
         self.hist.Draw()
         #self.hist.DrawCopy()
         #self.hist.SetFillColor(2)
@@ -97,6 +117,7 @@ def main():
     parser.add_argument('--from', '-f', type=float, default=0.0, help='Starting position.')
     parser.add_argument('--to', '-t', type=float, default=150000.0, help='Ending position.')
     parser.add_argument('--num-bins', '-n', type=int, default=200, help='The number of bins to analyze from starting to ending position.')
+    parser.add_argument('--title', help='Title for the plot')
     parser.add_argument('--debug', '-d', action='store_true', help='Print more status output')
 
     args = parser.parse_args()
@@ -118,23 +139,36 @@ def main():
     full_range = dargs['to'] - dargs['from']
     stepsize = full_range / (args.num_bins + 1)
 
-    values = []
+    total_values = []
+    by_material = dict()
     position = dargs['from'] + stepsize/2
-    while position <= args.to:
+    num_pos = 0
+    while position <= args.to - stepsize/2:
         budget = 0.0
         for comp in components:
-            budget += comp.budget_at(position)
+            component_budget = comp.budget_at(position)
+            budget += component_budget
+            try:
+                by_material[comp.material['name']]
+            except KeyError:
+                by_material[comp.material['name']] = []
+            try:
+                by_material[comp.material['name']][num_pos] += component_budget
+            except IndexError:
+                by_material[comp.material['name']].append(component_budget)
         if args.debug: print("Budget at the current position {:0.0f} is: {:0.2f}".format(position, budget))
-        values.append(budget)
+        total_values.append(budget)
         position += stepsize
+        num_pos += 1
 
-    non_zero_values = [value for value in values if value > 0.0]
+    non_zero_values = [value for value in total_values if value > 0.0]
     average_budget = sum(non_zero_values)/len(non_zero_values)
     print("The average material budget is {:0.2f}".format(average_budget))
 
     h = MatplotlibPlot(args.num_bins, dargs['from'], dargs['to'])
-    h.fill(values)
-    h.draw()
+    #h.fill({'total budget': total_values})
+    h.fill(by_material)
+    h.draw(title=args.title)
 
     raw_input()
 
