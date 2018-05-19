194512fc9ee1ed5b6c8ee632777da2779435363e
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-05-19 10:26:15 +0200

    getting rid of unused --fast

--- create-and-analyze-detector.py
+++ create-and-analyze-detector.py
@@ -166,7 +166,6 @@ def main():
     parser.add_argument('--num-x-bins', type=int, required=True, help='Number of bins in the x direction')
     parser.add_argument('--num-y-bins', type=int, required=True, help='Number of bins in the y direction')
     parser.add_argument('--samples-per-bin', type=int, help='Number of bins in the y direction')
-    parser.add_argument('--fast', action='store_true', help='Speed up imaging complicated SVGs with overlapping polygons of the same layer')
     parser.add_argument('json_geometry_file', help='The geometry file in JSON format')
     parser.add_argument('output_name', help='The basename for the output files')
     args = parser.parse_args()
